4552546fa8d2f26b8f58fd804410e0fe7d85e567
B-ricle <bnhenderson1106@gmail.com>
2025-03-24 00:32:45 -0400

    Added: array storing places in the finish line

---
+++ tst.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class tst {
+    public static void main(String[] args) {
+        System.out.println("This is test 1");
+
+        int num[] = { 2, 3, 4, 6, 7 };
+        int size = num.length;
+        int sum = 0;
+
+        for (int nums : num) {
+            System.out.print(nums + " ");
+        }
+
+    }
+}
